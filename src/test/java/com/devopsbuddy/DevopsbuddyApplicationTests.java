681ee004aaee9ecebbb3db5d286c0d7857deeec5
daniel ra <winra001@hotmail.com>
2017-02-05 08:52:12 +1100

    4-17. Setting up internationalisation with Spring Boot

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,27 @@
 package com.devopsbuddy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devopsbuddy.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18nService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
 	}
 
 }
