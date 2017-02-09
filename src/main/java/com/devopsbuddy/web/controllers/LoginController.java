f032bf53b11a68bbe444c31c4dc8f7fbe0b43012
daniel ra <winra001@hotmail.com>
2017-02-09 21:18:24 +1100

    5-22. Creating the login form

---
+++ src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	public static final String LOGIN_VIEW_NAME = "user/login";
+
+	@RequestMapping("/login")
+	public String login() {
+		return LOGIN_VIEW_NAME;
+	}
+
+}
