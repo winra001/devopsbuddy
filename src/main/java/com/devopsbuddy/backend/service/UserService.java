593831dc2bca5802f6c9db0a8ec20c46c352e5ca
daniel ra <winra001@hotmail.com>
2017-02-25 22:47:35 +1100

    10-49. Install MySQL locally

--- src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -9,9 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devopsbuddy.backend.persistence.domain.backend.PasswordResetToken;
 import com.devopsbuddy.backend.persistence.domain.backend.Plan;
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.persistence.repositories.PasswordResetTokenRepository;
 import com.devopsbuddy.backend.persistence.repositories.PlanRepository;
 import com.devopsbuddy.backend.persistence.repositories.RoleRepository;
 import com.devopsbuddy.backend.persistence.repositories.UserRepository;
@@ -35,29 +37,38 @@ public class UserService {
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
+	@Autowired
+	private PasswordResetTokenRepository passwordResetTokenRepository;
+
 	@Transactional
 	public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
-		String encryptedPassword = passwordEncoder.encode(user.getPassword());
-		user.setPassword(encryptedPassword);
+		User localUser = userRepository.findByEmail(user.getEmail());
 
-		Plan plan = new Plan(plansEnum);
-
-		// It makes sure the plans exist in the database
-		if (!planRepository.exists(plansEnum.getId())) {
-			plan = planRepository.save(plan);
+		if (localUser != null) {
+			LOG.info("User with username {} and email {} already exist. Nothing will be done. ", user.getUsername(), user.getEmail());
+		} else {
+			String encryptedPassword = passwordEncoder.encode(user.getPassword());
+			user.setPassword(encryptedPassword);
+	
+			Plan plan = new Plan(plansEnum);
+	
+			// It makes sure the plans exist in the database
+			if (!planRepository.exists(plansEnum.getId())) {
+				plan = planRepository.save(plan);
+			}
+	
+			user.setPlan(plan);
+	
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+	
+			user.getUserRoles().addAll(userRoles);
+	
+			localUser = userRepository.save(user);
 		}
 
-		user.setPlan(plan);
-
-		for (UserRole ur : userRoles) {
-			roleRepository.save(ur.getRole());
-		}
-
-		user.getUserRoles().addAll(userRoles);
-
-		user = userRepository.save(user);
-
-		return user;
+		return localUser;
 	}
 
 	/**
@@ -87,6 +98,11 @@ public class UserService {
 		password = passwordEncoder.encode(password);
 		userRepository.updateUserPassword(userId, password);
 		LOG.debug("Password updated successfully for user id {} ", userId);
+
+		Set<PasswordResetToken> resetTokens = passwordResetTokenRepository.findAllByUserId(userId);
+		if (!resetTokens.isEmpty()) {
+			passwordResetTokenRepository.delete(resetTokens);
+		}
 	}
 
 }
