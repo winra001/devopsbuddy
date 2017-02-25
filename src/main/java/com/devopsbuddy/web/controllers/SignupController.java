27cc04cdff60b4f86ea0e16022212d3691409f95
daniel ra <winra001@hotmail.com>
2017-02-25 12:38:38 +1100

    9-46. Introduction to the Pro Subscription User Journey

--- src/main/java/com/devopsbuddy/web/controllers/SignupController.java
+++ src/main/java/com/devopsbuddy/web/controllers/SignupController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -146,6 +147,18 @@ public class SignupController {
 			registeredUser = userService.createUser(user, PlansEnum.BASIC, roles);
 		} else {
 			roles.add(new UserRole(registeredUser, new Role(RolesEnum.PRO)));
+
+			// Extra precaution in case the POST method is invoked programmatically
+			if (StringUtils.isEmpty(payload.getCardCode()) ||
+					StringUtils.isEmpty(payload.getCardNumber()) ||
+					StringUtils.isEmpty(payload.getCardMonth()) ||
+					StringUtils.isEmpty(payload.getCardYear())) {
+				LOG.error("One or more credit card fields is null or empty. Returning error to the user");
+				model.addAttribute(SIGNED_UP_MESSAGE_KEY, "false");
+				model.addAttribute(ERROR_MESSAGE_KEY, "One of more credit card details is null or empty.");
+				return SUBSCRIPTION_VIEW_NAME;
+			}
+
 			registeredUser = userService.createUser(user, PlansEnum.PRO, roles);
 			LOG.debug(payload.toString());
 		}
